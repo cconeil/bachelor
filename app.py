04fcec7394f1898914037dd5f3040acd06aa21ab
Chris O'Neil <chris@robinhood.com>
2018-12-15 17:56:48 -0800

    got new models up

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_migrate import Migrate
 
 from models import db
-from models.models import User
+from models.models import Contestant
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db.db.init_app(app)
 
+migrate = Migrate(app, db.db)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello all users {}'.format(User.query.all())
+    return 'Hello all users!'
+
+
+@app.route('/update/')
+def return_everything():
+    contestants = Contestant.query.all()
+    return jsonify(contestants)
--- models/models.py
+++ models/models.py
@@ -1,13 +1,34 @@
-import db
+from db import db
 
-class User(db.db.Model):
-    id = db.db.Column(db.db.Integer, primary_key=True)
-    # id = db.Column(
-    #     db.UUID(as_uuid=True), unique=True, nullable=False, primary_key=True
-    # )
 
-    # username = db.Column(db.String(80), unique=True, nullable=False)
-    # email = db.Column(db.String(120), unique=True, nullable=False)
+class Contestant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256), nullable=False)
+    insta = db.Column(db.String(256), nullable=False)
+    image_url = db.Column(db.String(512), nullable=False)
+    elimated_date = db.Column(db.DateTime, nullable=True)
+    is_slops_crew = db.Column(db.Boolean)
+    data_points = db.relationship(
+        'FollowerCountDataPoint', backref='user', lazy=True
+    )
+
+    def __unicode__(self):
+        return '<User %r>' % self.id
+
+    def __repr__(self):
+        return '<User %r>' % self.id
+
+
+class FollowerCountDataPoint(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    num_followers = db.Column(db.String(256), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=True)
+    contestant_id = db.Column(
+        db.Integer, db.ForeignKey('contestant.id'), nullable=False
+    )
+
+    def __unicode__(self):
+        return '<User %r>' % self.id
 
     def __repr__(self):
         return '<User %r>' % self.id
