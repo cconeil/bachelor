92836e6a41c130d498e56dca8c579c67f3d9d51e
Billy Irwin <birwin93@gmail.com>
2018-12-15 20:27:16 -0800

    website

---
+++ website/build/precache-manifest.3996259281e50aa7f7adc9d750f25af6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ca5f98f78d5a98ab560",
+    "url": "/static/css/main.56fe7f4a.chunk.css"
+  },
+  {
+    "revision": "2ca5f98f78d5a98ab560",
+    "url": "/static/js/main.2ca5f98f.chunk.js"
+  },
+  {
+    "revision": "24f25e48fa0ebca381e6",
+    "url": "/static/js/1.24f25e48.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ce3f704bf5fb569d35df20d335f32b72",
+    "url": "/index.html"
+  }
+];
--- website/src/App.js
+++ website/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
 
   componentDidMount() {
     this.setState({ 'users': [] });
-    axios.get(`http://localhost:5000/bachelor_data`)
+    axios.get(`http://ec2-13-57-211-196.us-west-1.compute.amazonaws.com:3000/update`)
       .then(res => {
         this.setState({ "users": res.data });
       });
