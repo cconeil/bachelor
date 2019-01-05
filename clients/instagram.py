a3ae2d67dfa8de610789b18b910950d1e4419f09
Billy Irwin <birwin93@gmail.com>
2019-01-05 12:49:26 -0800

    stuff

--- clients/instagram.py
+++ clients/instagram.py
@@ -1,15 +1,24 @@
 from selenium import webdriver
+import os
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+DRIVER_PATH = os.path.join(PROJECT_ROOT, "chromedriver")
 
 class Instagram:
 
     def __init__(self):
-        self.service = webdriver.chrome.service.Service('./chromedriver')
-        options.add_argument('--headless')
-        options = options.to_capabilities()
-        driver = webdriver.Remote(service.service_url, options)
-
-    def start():
         options = webdriver.ChromeOptions()
         options.add_argument('--headless')
-        options = options.to_capabilities()
-        driver = webdriver.Remote(service.service_url, options)               
+        self.browser = webdriver.Chrome(executable_path=DRIVER_PATH, chrome_options=options)
+
+    def get_followers(self, username):
+        self.browser.get("https://instagram.com/" + username)
+        elements = self.browser.find_elements_by_partial_link_text('followers')
+        if len(elements) != 1:
+            return None
+
+        spans = elements[0].find_elements_by_tag_name('span')
+        if len(spans) != 1:
+            return None
+
+        return spans[0].get_attribute('title')
--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ import time
 import datetime
 from models.models import Contestant, FollowerCountDataPoint
 from models.db import db
+from clients.instagram import Instagram
 
 from app import app
 
@@ -14,8 +15,9 @@ token = 'EAAEeR6ST3pkBADSdXZBWIDsl9e0GbmKkN8wEd2iAbRmBAvECmoziS1wk2SMrgBsVWPCZCo
 url = "https://graph.facebook.com/v3.2/{}?fields=business_discovery.username({})%7Bfollowers_count%2Cmedia_count%7D&access_token={}"
 
 
-def _get_follower_count(username):
+def _get_follower_count(username, instagram):
     try:
+        count = instagram.get_followers
         res = requests.get(url.format(account_id, username, token))
         print(res.json())
         return res.json()['business_discovery']['followers_count']
@@ -26,17 +28,29 @@ def _get_follower_count(username):
 @manager.command
 def update_follower_counts():
     contestants = Contestant.query.all()
+    instagram = Instagram()
 
     for contestant in contestants:
-        follower_count = _get_follower_count(contestant.insta)
-        if follower_count:
-            data_point = FollowerCountDataPoint(
-                num_followers=follower_count, timestamp=datetime.datetime.now()
-            )
-            contestant.data_points.append(data_point)
-            db.session.add(contestant)
+        follower_count_str = instagram.get_followers(contestant.insta)
+        if follower_count_str == None:
+            print("Something went wrong for", contestant.insta)
+            continue
+
+        follower_count = follower_count_str.replace(',', '')
+        print(follower_count)
+
+        data_point = FollowerCountDataPoint(
+            num_followers=follower_count, timestamp=datetime.datetime.now()
+        )
+        contestant.data_points.append(data_point)
+        db.session.add(contestant)
 
     db.session.commit()
+
+@manager.command
+def load_insta():
+    instagram = Instagram()
+    instagram.load_site()
 
 if __name__ == "__main__":
     manager.run()
